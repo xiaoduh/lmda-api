d2b3e6a011e1245d3b057464ccbb34a4b1702006
ClémentL <clement.lidar@gmail.com>
2023-10-03 21:07:13 +0200

    routing

--- routes/business.routes.js
+++ routes/business.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "voici les prise de contact pour un profil !!!" });
+  res.json({ message: "voici les prise de contact pour un profil !!!!" });
 });
 
 router.post("/", (req, res) => {
