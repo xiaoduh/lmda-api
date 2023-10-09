04b9f2592da342accf4286fedf32f16fbf40d87d
ClémentL <clement.lidar@gmail.com>
2023-10-09 21:38:27 +0200

    add user to bdd

---
+++ utils/sender.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+
+const sender = async (email, subject, data) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "therealbigdeeel@gmail.com",
+        pass: "mmazhfocikkdkcsl",
+      },
+    });
+
+    await transporter.sendMail({
+      from: "no-reply lambda",
+      to: email,
+      subject: subject,
+      html: `<body>
+      <p>Bonjour 👋,<br><br></p>
+      <p>Vous avez un nouvel abonné à votre liste de diffusion :</p>
+      <p>${data}</p>
+      </body>`,
+    });
+
+    console.log("email sent successfully");
+  } catch (err) {
+    console.log("email not sent" + err);
+  }
+};
+
+module.exports = sender;
