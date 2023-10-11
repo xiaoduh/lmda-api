f43c243b2d47365c2bf8d4680e8b098ed7f894c6
clement <clement.lidar@gmail.com>
2023-10-11 14:14:26 +0200

    appliance nodemailer

--- controllers/career.controllers.js
+++ controllers/career.controllers.js
@@ -1,16 +1,21 @@
 const CareerModel = require("../models/career.model");
+const appliance = require("../utils/appliance");
 
 module.exports.createCareerRequest = async (req, res) => {
-  const { subject, email, phone_number, motivation } = req.body;
-
   try {
     const newCareerRequestEntry = await CareerModel.create({
-      subject: subject,
-      email: email,
-      phone_number: phone_number,
-      motivation: motivation,
+      subject: req.body.subject,
+      email: req.body.email,
+      phone_number: req.body.phone_number,
+      motivation: req.body.motivation,
     });
-    res.status(200).json(newCareerRequestEntry);
+    appliance(
+      req.body.subject,
+      req.body.email,
+      req.body.phone_number,
+      req.body.motivation
+    );
+    res.status(200).json({ newAppliance: newCareerRequestEntry });
   } catch (error) {
     res.status(500).json(error);
   }
