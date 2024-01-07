77a391b9c8c00a061ac2188e89195a504371bf32
xiaoduh <83875368+xiaoduh@users.noreply.github.com>
2024-01-07 22:47:39 +0100

    Delete routes/mission.routes.js

--- routes/mission.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const missionControllers = require("../controllers/mission.controllers");
-
-router.get("/", missionControllers.getMissionRequests);
-
-router.post("/", missionControllers.createMissionRequest);
-
-module.exports = router;
