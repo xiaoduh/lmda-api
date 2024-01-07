dcc09ca428376ebb99a33c26cd02884ef8e82bb0
xiaoduh <83875368+xiaoduh@users.noreply.github.com>
2024-01-07 22:47:44 +0100

    Delete routes/subscribers.routes.js

--- routes/subscribers.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const subControllers = require("../controllers/subscriber.controllers");
-
-router.get("/", subControllers.getAllSubscribers);
-
-router.post("/", subControllers.addNewSubscriber);
-
-module.exports = router;
